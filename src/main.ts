9747dc9185c08fb6aac331af5b015d39e5b590ce
cerdikdev <cerdikdev@gmail.com>
2022-08-25 13:11:56 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,71 @@
+import express from "express";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import cors, { CorsRequest } from "cors";
+import bodyParser from "body-parser";
+import gql from "graphql-tag";
+import { createServer, Server } from "http";
+import { config } from "dotenv";
+
+config();
+const port = process.env.PORT || 4000;
+const app = express();
+const httpServer = createServer(app);
+
+interface MyContext {}
+
+const apolloServer = new ApolloServer<MyContext>({
+  typeDefs: gql`
+    type Tes {
+      name: String
+    }
+    type Query {
+      getTes: Tes
+    }
+  `,
+  resolvers: {
+    Query: {
+      getTes: () => {
+        return {
+          name: "tes",
+        };
+      },
+    },
+  },
+  plugins: [ApolloServerPluginLandingPageLocalDefault()],
+});
+
+async function start() {
+  /* load apollo server */
+  await apolloServer.start();
+
+  /* add apollo middleware */
+  app.use(
+    "/graphql",
+    bodyParser.json(),
+    cors<CorsRequest>(),
+    expressMiddleware(apolloServer, {
+      context: async (context) => {
+        return {
+          ...context,
+        };
+      },
+    })
+  );
+
+  const run = await new Promise<Server>((resolve) =>
+    httpServer.listen(port, () => resolve.call(null, httpServer))
+  );
+  return () =>
+    new Promise((res) =>
+      run.close(() => {
+        console.log("server has been closed");
+
+        res(null);
+      })
+    );
+}
+
+await start();
+console.log(`server run on port 4000`);
